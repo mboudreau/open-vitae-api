bac1b58f416e603d22e8b9a29d734364551a6d3e
Michel Boudreau <michelb@pageuppeople.com>
2016-03-07 15:15:13 +1100

    fixing some object issues

--- src/routes/user.js
+++ src/routes/user.js
@@ -15,7 +15,7 @@ module.exports = function (server) {
 	});
 
 	server.post('/user/:email', function (req, res, next) {
-		logger.info("user with email " + req.params.email + " with results: " + req.body);
+		logger.info("user with email " + req.params.email + " with results: " + JSON.stringify(req.body));
 		var answers = req.body.answers;
 		var answer;
 		var table = dynasty.table('open-vitae');
@@ -38,19 +38,19 @@ module.exports = function (server) {
 			answer = answers[i];
 			switch (true) {
 				case contains(answer.tags, 'name'):
-					resume.name = answer.value;
+					resume.basics.name = answer.value;
 					break;
 				case contains(answer.tags, 'address') && contains(answer.tags, 'street'):
-					resume.location.address = answer.value;
+					resume.basics.location.address = answer.value;
 					break;
 				case contains(answer.tags, 'address') && contains(answer.tags, 'suburb'):
-					resume.location.city = answer.value;
+					resume.basics.location.city = answer.value;
 					break;
 				case contains(answer.tags, 'address') && contains(answer.tags, 'state'):
-					resume.location.region = answer.value;
+					resume.basics.location.region = answer.value;
 					break;
 				case contains(answer.tags, 'address') && contains(answer.tags, 'postcode'):
-					resume.location.postalCode = answer.value;
+					resume.basics.location.postalCode = answer.value;
 					break;
 				case contains(answer.tags, 'employer') && contains(answer.tags, 'emp_name'):
 					employer = {};
@@ -99,5 +99,8 @@ module.exports = function (server) {
 
 
 function contains(array, element) {
-	return array.indexOf(element) > -1;
+	if(array) {
+		return array.indexOf(element) > -1;
+	}
+	return false;
 }
